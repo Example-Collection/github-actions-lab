77cd7c10b8dcaae19b29e56792f4c07a69c220a1
sang-w0o <robbyra@gmail.com>
2022-09-11 00:40:33 +0900

    fix wrong git global config

--- some.sh
+++ some.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# git config --global user.email "robbyra@gmail.com"
-# git config --global user.password $GHB_PAT
+git config --global user.email "robbyra@gmail.com"
+git config --global user.password $GHB_PAT
 
 mkdir DEVOPS_FOLDER
 git clone $DEVOPS_REPO_URL ./DEVOPS_FOLDER
