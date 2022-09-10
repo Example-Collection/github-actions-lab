e649ed637f82d9aebf8a9bd613b7218855bf0ef7
sang-w0o <robbyra@gmail.com>
2022-09-11 00:47:48 +0900

    point to devops_repo_url

--- some.sh
+++ some.sh
@@ -5,13 +5,15 @@ git config --global user.name "sang-w0o"
 git config --global user.password $GHB_PAT
 
 mkdir DEVOPS_FOLDER
+cd ./DEVOPS_FOLDER
 git clone $DEVOPS_REPO_URL ./DEVOPS_FOLDER
-cd ./DEVOPS_FOLDER/k8s
+cd ./k8s
 
 # line to change: image: 598334522273.dkr.ecr.ap-northeast-2.amazonaws.com/planit_product:*
 # change image tag to $COMMIT_HASH
 sed -i "s/598334522273.dkr.ecr.ap-northeast-2.amazonaws.com\/planit_product:.*/598334522273.dkr.ecr.ap-northeast-2.amazonaws.com\/planit_product:$COMMIT_HASH/" a.yaml
 
+git remote set-url origin $DEVOPS_REPO_URL
 git checkout -b "deploy/$COMMIT_HASH"
 git add .
 git commit -m "Deploy product image(tag: $COMMIT_HASH)"
